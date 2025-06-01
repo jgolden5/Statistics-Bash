d4f06ea7237f85bbca4a26b525f7c99f7b4dc6eb
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-06-01 13:57:00 -0700

    first commit

---
+++ mean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+data=("$@")
+total=0
+for e in "${data[@]}"; do
+  if [[ $e =~ [0-9] ]]; then
+    total="$(echo "$total + $e" | bc)"
+  else
+    exit 1
+  fi
+done
+mean="$(echo "$total / ${#data[@]}" | bc)"
+echo "Mean = $mean"
